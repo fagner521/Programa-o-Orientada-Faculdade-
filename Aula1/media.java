548c3ff9bfb3d79b5b1bf3eb578ea6551a9e8617
fagneralves <fagner.all@hotmail.com>
2021-08-16 09:58:40 -0300

    Adicionando nota de final.

--- Aula1/media.java
+++ Aula1/media.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 public class media{
 
      public static void main(String []args){
-        double nota1 = 0, nota2 = 0, media = 0;
+        double nota1 = 0, nota2 = 0, media = 0, notafinal = 0;
         System.out.printf("Digite as notas\n");
         Scanner s = new Scanner(System.in);
         nota1 = s.nextDouble();
         nota2 = s.nextDouble();
         
         media = (nota1+nota2)/2;
+        notafinal = (10 - media);
 
         if(media >= 7){
             System.out.println("Aprovado por media");
         }else if(media >= 4 && media < 7){
             System.out.println("Precisa fazer prova final");
+            System.out.println("Precisa tirar: " + notafinal);
         }else if(media < 4){
             System.out.println("Reprovado");
         }
