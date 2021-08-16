ad5f879066df304f08514dae773c4421000e33b6
fagneralves <fagner.all@hotmail.com>
2021-08-16 09:44:56 -0300

    Exercício pedido pelo professor, para calcular a media.

---
+++ Aula1/media.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class media{
+
+     public static void main(String []args){
+        double nota1 = 0, nota2 = 0, media = 0;
+        System.out.printf("Digite as notas\n");
+        Scanner s = new Scanner(System.in);
+        nota1 = s.nextDouble();
+        nota2 = s.nextDouble();
+        
+        media = (nota1+nota2)/2;
+
+        if(media >= 7){
+            System.out.println("Aprovado por media");
+        }else if(media >= 4 && media < 7){
+            System.out.println("Precisa fazer prova final");
+        }else if(media < 4){
+            System.out.println("Reprovado");
+        }
+        
+        // System.out.printf("A média é: %f \n",media);
+        System.out.println("A média é: " + media);
+        
+     }
+}
