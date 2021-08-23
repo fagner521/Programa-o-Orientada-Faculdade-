0f8fe31bd935860f7a707e8ee4cbdeedea5f9b40
fagneralves <fagner.all@hotmail.com>
2021-08-23 16:26:41 -0300

    Exercicio 4 Ver 2

--- Aula2/Eudoxo.java
+++ Aula2/Eudoxo.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 public class Eudoxo{
     public static void main(String []Args){
-        double raiz = 0, iteracao = 0, c = 0, x = 0;
+        double raiz = 0, iteracao = 0, c = 0, x = 0, erroab = 0, real = 0;
         System.out.print("\033[H\033[2J");  
         System.out.flush();
-        System.out.print("Qual a raiz você quer calcular ? :");
+        System.out.printf("Qual a raiz você quer calcular ? :");
         Scanner s = new Scanner(System.in);
         c = s.nextDouble();
+        real = Math.sqrt(c);
         x = (1 + c)/2;
-        System.out.print("n "+iteracao +" = "+ x + "\n");
-        while (iteracao < 9){
+        erroab = real - x;
+        System.out.printf("n "+iteracao +" = "+ x +" "+Math.abs(erroab)+ "\n");
+        while ( Math.abs(erroab) > 0.00001){
             x = ((c/x)+x)/2;
             iteracao = iteracao + 1;
-            System.out.print("n "+iteracao +" = "+ x + "\n");
+            erroab = real - x;
+            System.out.printf("n %.1f = %.2f %.5f\n", iteracao, x, Math.abs(erroab));
         }
+        System.out.printf("By SoenShem")
     }
 }
